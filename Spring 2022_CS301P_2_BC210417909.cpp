4f9f364b4e58b5261d0f5105c9a9c354f438c982
azeemvucs <112061559+azeemvucs@users.noreply.github.com>
2022-08-31 10:44:33 +0500

    hfghg
    ghghg

---
+++ Spring 2022_CS301P_2_BC210417909.cpp
@@ -0,0 +1,77 @@
+// Online C++ compiler to run C++ program online
+
+ #include <iostream>
+
+ using namespace std;
+
+ class Heap {
+    private:
+    int currentSize;
+    int *array;
+    int capacity;
+    public:
+    Heap (int size){
+        
+        bool insert (const int& x){
+            bool isEmpty (){
+                
+                bool isFull(){
+                    
+                    void traverse(){
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+ };
+
+
+    class BSTNode{
+        private:
+        int object;
+        BSTNode* left;
+        BSTNode* right;
+        public:
+        BSTNode(int obj){
+            object = obj;
+            left = NULL;
+            right = NULL;
+        }
+            void setInfo(int obj){
+                object = obj;
+            }
+        int getInfo(){
+            return object;
+        }
+        void setLeft(BSTNode* left){
+            this->lect = left;
+        }
+        BSTNode* getLeft(){
+            return this-> left;
+        }
+        void setRight(BSTNode* right){
+            this-> right = right;
+        }
+        BSTNode* getRight(){
+            return this-> right;
+        }
+        
+         };
+         
+         BSTNode* insertNode(BSTNode* node, int data){
+             BSTNode* buildTree(BSTNode8 node, int array[], int size){
+                 void inorder(BSTNode* root, Heap* heap){
+                     
+                 }
+             }
+         }
+         
+    int main(){
+        int arr[] = {18, 31, 82, 85, 37, 20, 23, 79, 47};
+        int size = sizeof(arr)/sizeof(arr[0]);
+        BSTNode* root = NULL;
+        Heap* heap =  new Heap(size);
+    }
+    
